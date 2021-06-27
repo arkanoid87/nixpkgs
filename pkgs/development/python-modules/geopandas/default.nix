357cc3e07c9e4aaaae26c10a7c4610b4f45838c2
arkanoid87 <arkanoid87@users.noreply.github.com>
2021-06-27 22:42:37 +0200

    python-modules geopandas: bump to 0.9.0
    removed descartes dependency: not required (and broken in nixpkgs) https://geopandas.org/getting_started/install.html
    removed patches: not required anymore
    checks are OK: 1200 passed, 84 skipped, 2 deselected, 19 xfailed, 2 xpassed, 161 warnings

--- pkgs/development/python-modules/geopandas/default.nix
+++ pkgs/development/python-modules/geopandas/default.nix
@@ -1,39 +1,23 @@
 { lib, stdenv, buildPythonPackage, fetchFromGitHub, isPy27
-, pandas, shapely, fiona, descartes, pyproj
+, pandas, shapely, fiona, pyproj
 , pytestCheckHook, Rtree, fetchpatch }:
 
 buildPythonPackage rec {
   pname = "geopandas";
-  version = "0.8.1";
+  version = "0.9.0";
   disabled = isPy27;
 
   src = fetchFromGitHub {
     owner = "geopandas";
     repo = "geopandas";
     rev = "v${version}";
-    sha256 = "0618p0s0biisxk2s0h43hkc3bs1nwjk84rxbfyd6brfvs9yx4vq7";
+    sha256 = "18bdx90gf1170j6b9ddrhv75qkp1sq2lyk1ka5w3cix4cgmzkig7";
   };
-
-  patches = [
-    # Fix for test test_numerical_operations: https://github.com/geopandas/geopandas/issues/1541
-    (fetchpatch {
-      url = "https://github.com/geopandas/geopandas/pull/1544/commits/6ce868a33a2f483b071089d51e178030fa4414d0.patch";
-      sha256 = "1sjgxrqgbhz5krx51hrv230ywszcdl6z8q3bj6830kfad8n8b5dq";
-    })
-    # Fix GeoJSON for Fiona>=1.8.16 (Sep. 7, 2020).
-    # https://github.com/geopandas/geopandas/issues/1606
-    # Will be included in next upstream release after 0.8.1
-    (fetchpatch {
-      url = "https://github.com/geopandas/geopandas/commit/72427d3d8c128039bfce1d54a76c0b652887b276.patch";
-      sha256 = "1726mrpddgmba0ngff73a5bsb6ywpsg63a2pdd2grp9339bgvi4a";
-    })
-  ];
 
   propagatedBuildInputs = [
     pandas
     shapely
     fiona
-    descartes
     pyproj
   ];
 
